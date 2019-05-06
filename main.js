d934b7a46f7a74253f46201baf877f1ad4f8d13b
Jimmy Tan <tanjimmy1129@gmail.com>
2019-05-06 11:24:40 -0400

    let answers =

--- main.js
+++ main.js
@@ -6,11 +6,15 @@ console.log('Hello Lil Mac')
 //   console.log(i + 1)
 //   i +=1
 
-let answer = prompt('How are you doing?').toLowerCase()
-goodWords = ['good,' 'well', 'nice', 'fair', 'yer']
-badWords = ['chill', 'yer',]
 
-if (answer == "good"){
+
+let answer = prompt('How are you doing?').toLowerCase()
+let goodWords = ['good', 'well', 'nice', 'fair', 'yer']
+let badWords = ['chill', 'bad', 'meh', 'i fought roy']
+
+ // if ... goodWords.includes(answer)
+
+if (goodWords.includes(answer)){
   console.log('That\'s great')
   let answer2 = prompt('Where are you from?').toLowerCase()
   if(answer2 == 'smashville') {
